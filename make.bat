44a5dc09a9f7c513462417dbdec466bf3dce03e3
Eqnoxx <81754864+Eqno@users.noreply.github.com>
2022-03-03 16:25:55 +0800

    Add Makefile
    Add Makefile to complie the game faster

--- make.bat
+++
@@ -1,8 +0,0 @@
-g++ -o EqnoCraft.exe ^
-src/setup.cpp src/window.cpp src/utils.cpp ^
-src/init.cpp src/texture.cpp src/transform.cpp ^
-src/render.cpp src/view.cpp src/collision.cpp ^
-src/pointer.cpp src/coord.cpp src/block.cpp ^
-src/bedrock.cpp src/ground.cpp ^
-src/listener.cpp src/timer.cpp ^
--Wall -O2 -D FREEGLUT_STATIC -static-libgcc -static -std=c++17 -lfreeglut_static -lopengl32 -lwinmm -lgdi32 -lglu32
